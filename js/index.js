5385b6df33847fee9008ebfc4196025c1b2eb07e
erkang1 <51319096+erkang1@users.noreply.github.com>
2022-08-22 18:19:07 +0800

    Upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+const apkUrl = 'https://dl.d0d2d.xyz/games/af2b4/com.pokervages.game/PokerVegas_wap_v3.3.13_v3.3.13_3313.apk'
+
+
+function installClick(flag) {
+  window.location.href = apkUrl
+}

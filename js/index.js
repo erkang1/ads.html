8063cbb89b7b14b8ca4db75cd5225713c937d9a8
erkang1 <51319096+erkang1@users.noreply.github.com>
2022-09-02 11:57:29 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-const apkUrl = 'https://dl.d0d2d.xyz/games/af2b4/com.pokervages.game/PokerVegas_wap_v3.3.13_v3.3.13_3313.apk'
+const apkUrl = `https://dl.d0d2d.xyz/games/af2b4/com.pokervages.game/PokerVegas_wap_v3.4.13_v3.4.13_3413.apk`
 
 
 function installClick(flag) {
